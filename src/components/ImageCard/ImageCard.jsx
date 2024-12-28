7627ed58046535ff9499d025c3b20065bcae9cbe
Liza <lizaapostol378@gmail.com>
2024-12-28 20:37:02 +0200

    update onClick

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,12 @@ import styles from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onClick }) => {
   return (
-    <div onClick={onClick} className={styles.imageCardWrap}>
+    <div className={styles.imageCardWrap}>
       <img
         src={image.urls.small}
         alt={image.alt_description}
         className={styles.image}
+        onClick={onClick}
       />
     </div>
   );
